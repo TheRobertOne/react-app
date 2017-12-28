513068e0d98054d41cea55e7bf9a50a4dae602f5
pingyi Luo <pingyiluo@gmail.com>
2017-12-28 11:23:31 +0800

    update

--- src/router/root-router.js
+++ src/router/root-router.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import {
     BrowserRouter as Router,
-    Route
+    Route,
+    Link
 } from 'react-router-dom'
 import App from '../component/app';
 import SayHello from '../component/say-hello';
 class RootRouter extends Component {
-   
+
     render() {
         return (
             <Router>
                 <div>
+                    <ul>
+                        <li><Link to="/">home</Link></li>
+                        <li><Link to="/say">say</Link></li>
+                    </ul>
                     <Route exact path="/" component={App} />
                     <Route path="/say" component={SayHello} />
                 </div>
