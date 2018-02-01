dc5f40fa74b7acc58f5ebe8ee5f51ea72b903604
pingyi Luo <pingyiluo@gmail.com>
2018-02-01 23:21:40 +0800

    update

--- src/component/login/register.js
+++ src/component/login/register.js
@@ -43,26 +43,25 @@ class Register extends Component {
         mobile = (mobile || '').trim();
         password = (password || '').trim();
         username = (username || '').trim();
-        // if (validator.isEmptyString(mobile)) {
-        //     message.warning('请输入手机号码!');
-        //     return;
-        // }
-        // if (validator.isEmptyString(password)) {
-        //     message.warning('请输入密码!');
-        //     return;
-        // }
-        // if (validator.isEmptyString(username)) {
-        //     message.warning('请输入名号!');
-        //     return;
-        // }
+        if (validator.isEmptyString(mobile)) {
+            message.warning('请输入手机号码!');
+            return;
+        }
+        if (validator.isEmptyString(password)) {
+            message.warning('请输入密码!');
+            return;
+        }
+        if (validator.isEmptyString(username)) {
+            message.warning('请输入名号!');
+            return;
+        }
 
-        network('hhh').post('/register', {
+        network().post('/register', {
             mobile,
             password,
             username
         }, (json) => {
 
-            console.log(json);
             message.success("注册成功");
         }, (err) => {
             message.error(err.message);
