0427cdfe3a10d605a38610b1a784a9b5330d5e80
pingyi Luo <pingyiluo@gmail.com>
2018-03-26 00:14:02 +0800

    up

--- src/component/DataArr/Choice.js
+++ src/component/DataArr/Choice.js
@@ -1,16 +1,169 @@
 import React, { Component } from 'react';
-
+import actionTypes from '../../reducer/action-types';
 import DeleteItem from './DeleteItem';
+import { Input, Button, Checkbox } from 'antd';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 class Choice extends Component {
+    constructor() {
+        super();
+        this.state = {
+            checkedTextX: false
+        }
+    }
+    //改变图片
+    changeImage = (dataBodyItem, e) => {
+        let { initData } = this.props;
+        let val = e.target.value;
+        dataBodyItem['image'] = val;
+        this.props.dispatch({
+            type: actionTypes.HEADER_CHAGNE_COURSEWARE,
+            payload: initData['courseware']
+        });
+    }
+    //
+    onBlurChangeImage = (dataBodyItem, e) => {
+        let { initData } = this.props;
+        let val = e.target.value;
+        dataBodyItem['image'] = (val || '').trim();
+        this.props.dispatch({
+            type: actionTypes.HEADER_CHAGNE_COURSEWARE,
+            payload: initData['courseware']
+        });
+    }
+    //改变pos.x坐标
+    changePosX = (pos, e) => {
+        let { initData } = this.props;
+        let val = e.target.value;
+        pos['x'] = val;
+        this.props.dispatch({
+            type: actionTypes.HEADER_CHAGNE_COURSEWARE,
+            payload: initData['courseware']
+        });
+    }
+    //
+    onBlurChangePosX = (pos, e) => {
+        let { isCenterX } = this.state;
+        let { initData } = this.props;
+        let val = (e.target.value || '').trim();
+        val = parseFloat(val);
+        val = isNaN(val) ? 0 : val;
 
+        if (isCenterX) {
+            pos['x'] = (1024 - val) / 2.00;
+        } else {
+            pos['x'] = val;
+        }
 
+        this.props.dispatch({
+            type: actionTypes.HEADER_CHAGNE_COURSEWARE,
+            payload: initData['courseware']
+        });
+    }
+    //改变pos.y坐标
+    changePosY = (pos, e) => {
+        let { initData } = this.props;
+        let val = e.target.value;
+        pos['y'] = val;
+        this.props.dispatch({
+            type: actionTypes.HEADER_CHAGNE_COURSEWARE,
+            payload: initData['courseware']
+        });
+    }
+    //
+    onBlurChangePosY = (pos, e) => {
+        let { initData } = this.props;
+        let val = (e.target.value || '').trim();
+        val = parseFloat(val);
+        val = isNaN(val) ? 0 : val;
+        pos['y'] = val;
+        this.props.dispatch({
+            type: actionTypes.HEADER_CHAGNE_COURSEWARE,
+            payload: initData['courseware']
+        });
+    }
+    //居中pos.x坐标
+    centerX = (e) => {
+        let checked = e.target.checked;
+        this.setState({
+            isCenterX: checked
+        });
+    }
+    //删除图片
+    delBodyImg = (item) => {
+
+        let { initData, data } = this.props;
+        let body = data['data']['body'];
+
+        let len = body.length;
+        let newArr = [];
+        for (let i = 0; i < len; i++) {
+            if (body[i] !== item) {
+                newArr.push(body[i]);
+            }
+        }
+        data['data']['body'] = newArr;
+        let courseware = initData['courseware'];
+        courseware[data['page']] = data;
+
+        this.props.dispatch({
+            type: actionTypes.HEADER_DISPLAY_BODY_IMG_CHAGEN,
+            payload: courseware
+        });
+    }
+    //添加图片
+    addBodyImg = (dataBody) => {
+
+        let { initData, data } = this.props;
+        let body = data['data']['body'];
+        let imgObj = { "image": "", "pos": { "x": 0, "y": 90 } };
+        body.push(imgObj);
+        data['data']['body'] = body;
+        let courseware = initData['courseware'];
+        courseware[data['page']] = data;
+
+        this.props.dispatch({
+            type: actionTypes.HEADER_DISPLAY_BODY_IMG_CHAGEN,
+            payload: courseware
+        });
+    }
     render() {
         let { data } = this.props;
         return (
             <div className="topic-item-box">
                 <DeleteItem item={data} />
-                Choice
+                <div>
+                    <div className="display-body-title">
+                        <span>data.body</span>
+                        <Button type="primary" onClick={this.addBodyImg.bind(this, data['data']['body'])}>添加图片</Button>
+                    </div>
+                    {data['data']['body'].map((item, index) => {
+                        return (
+                            <div key={index} className="display-image-box">
+                                <div>索引:{index}</div>
+                                <div className="image-item">
+                                    <span >image:</span>
+                                    <Input value={item['image']} onChange={this.changeImage.bind(this, item)} onBlur={this.onBlurChangeImage.bind(this, item)} />
+                                </div>
+                                <div className="image-item">
+                                    <span >pos:x</span>
+                                    <Input value={item['pos']['x']} onChange={this.changePosX.bind(this, item['pos'])} onBlur={this.onBlurChangePosX.bind(this, item['pos'])} />
+                                </div>
+                                <div className="image-item">
+                                    <span >pos:y</span>
+                                    <Input value={item['pos']['y']} onChange={this.changePosY.bind(this, item['pos'])} onBlur={this.onBlurChangePosY.bind(this, item['pos'])} />
+                                </div>
+                                <div className="image-item">
+                                    <Checkbox onChange={this.centerX} checked={this.state.isCenterX}>居中x</Checkbox>
+                                </div>
+                                <div className="image-item">
+                                    <Button type="primary" onClick={this.delBodyImg.bind(this, item)}>删除图片</Button>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
 
         );
@@ -18,4 +171,22 @@ class Choice extends Component {
 
 }
 
-export default Choice;
+function mapStateToProps(state, ) {
+    let initData = state['header'].get('initData').toJS();
+    return {
+        initData
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    let method = {
+    };
+    let boundActionCreators = bindActionCreators(method, dispatch);
+    return {
+        dispatch,
+        ...boundActionCreators
+    }
+
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Choice);
