fda78b88ee782ee971c53a617808765bf4fec216
pingyi Luo <pingyiluo@gmail.com>
2018-03-25 23:56:26 +0800

    update

--- src/component/DataArr/Read.js
+++ src/component/DataArr/Read.js
@@ -77,10 +77,79 @@ class Read extends Component {
             payload: initData['courseware']
         });
     }
+    textPosY = (pos, e) => {
+        let { initData } = this.props;
+        let val = e.target.value;
+        pos['y'] = val;
+        this.props.dispatch({
+            type: actionTypes.HEADER_CHAGNE_COURSEWARE,
+            payload: initData['courseware']
+        });
+    }
+    blurTextPosY = (pos, e) => {
+        let { initData } = this.props;
+        let val = (e.target.value || '').trim();
+        val = parseFloat(val);
+        val = isNaN(val) ? 0 : val;
+
+        pos['y'] = val;
+
+        this.props.dispatch({
+            type: actionTypes.HEADER_CHAGNE_COURSEWARE,
+            payload: initData['courseware']
+        });
+    }
     checkedTextXChange = (e) => {
         let checked = e.target.checked;
         this.setState({
             checkedTextX: checked
+        });
+    }
+    delTextImage = () => {
+        let { initData, data } = this.props;
+        data['data']['text_Image'] = '';
+        let courseware = initData['courseware'];
+        courseware[data['page']] = data;
+
+        this.props.dispatch({
+            type: actionTypes.HEADER_DISPLAY_BODY_IMG_CHAGEN,
+            payload: courseware
+        });
+    }
+    changeImage = (e) => {
+        let { initData, data } = this.props;
+        let val = e.target.value;
+        data['data']['image'] = val;
+        let courseware = initData['courseware'];
+        courseware[data['page']] = data;
+
+        this.props.dispatch({
+            type: actionTypes.HEADER_DISPLAY_BODY_IMG_CHAGEN,
+            payload: courseware
+        });
+    }
+    blurChangeImage = (e) => {
+        let { initData, data } = this.props;
+        let val = e.target.value;
+        val = (val || '').trim();
+        data['data']['image'] = val;
+        let courseware = initData['courseware'];
+        courseware[data['page']] = data;
+
+        this.props.dispatch({
+            type: actionTypes.HEADER_DISPLAY_BODY_IMG_CHAGEN,
+            payload: courseware
+        });
+    }
+    delImage = () => {
+        let { initData, data } = this.props;
+        data['data']['image'] = '';
+        let courseware = initData['courseware'];
+        courseware[data['page']] = data;
+
+        this.props.dispatch({
+            type: actionTypes.HEADER_DISPLAY_BODY_IMG_CHAGEN,
+            payload: courseware
         });
     }
     render() {
@@ -109,30 +178,27 @@ class Read extends Component {
                         </div>
                         <div className="image-item">
                             <span >text_pos:y</span>
-                            <Input />
+                            <Input value={data['data']['text_pos']['y']} onChange={this.textPosY.bind(this, data['data']['text_pos'])} onBlur={this.blurTextPosY.bind(this, data['data']['text_pos'])} />
                         </div>
                         <div className="image-item">
-                            <Button type="primary" >删除text_Image图片</Button>
+                            <Button type="primary" onClick={this.delTextImage}>删除text_Image图片</Button>
                         </div>
                     </div>
                     <div className="display-image-box">
                         <div className="image-item">
                             <span >image:</span>
-                            <Input />
+                            <Input value={data['data']['image']} onChange={this.changeImage} onBlur={this.blurChangeImage} />
                         </div>
                         <div className="image-item">
                             <span >pos:x</span>
-                            <Input />
-                        </div>
-                        <div className="image-item">
-                            <Checkbox >居中x</Checkbox>
+                            <Input value={data['data']['pos']['x']} />
                         </div>
                         <div className="image-item">
                             <span >pos:y</span>
-                            <Input />
+                            <Input value={data['data']['pos']['y']} />
                         </div>
                         <div className="image-item">
-                            <Button type="primary" >删除image图片</Button>
+                            <Button type="primary" onClick={this.delImage}>删除image图片</Button>
                         </div>
                     </div>
                 </div>
