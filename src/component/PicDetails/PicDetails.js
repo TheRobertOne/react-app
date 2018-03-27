07fe81252023fcc1b6771df5e909ea7461c96981
EasyValley <pingyiluo@gmail.com>
2018-03-27 11:03:57 +0800

    u

--- src/component/PicDetails/PicDetails.js
+++ src/component/PicDetails/PicDetails.js
@@ -30,16 +30,20 @@ class PicDetails extends Component {
     render() {
         
         let data = this.state.data;
+        let env  = process.env.NODE_ENV;
+
+       
         return (
             <div className='pic-detail-box'>
-                <div className="header"><Button type="primary" className="btn" onClick={this.handleClick}>刷新图片</Button></div>
+                <div className="header"><Button type="primary" className="btn" onClick={this.handleClick}>刷新图片jj</Button></div>
                 <div className="content">
                     {data.map((item, index) => {
+                        let url = env==='development'?'api'+item['url']:item['url'];
                         return (<div className="content-item" key={index}>
                             <span>{item['name']}</span>
                             <span><i>宽:</i>{item['w']}</span>
                             <span><i>高:</i>{item['h']}</span>
-                            <span className="img-span"><img src={item['url']} key={index} alt=''/></span>
+                            <span className="img-span"><img src={url} key={index} alt=''/></span>
                         </div>);
                     })}
 
